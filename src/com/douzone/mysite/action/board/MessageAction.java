52bf5fec790e7ea9611963bf2fe0227bdbb32a8d
cogud0908 <cogud0908@naver.com>
2019-01-31 19:02:48 +0900

    input exception

--- src/com/douzone/mysite/action/board/MessageAction.java
+++ src/com/douzone/mysite/action/board/MessageAction.java
@@ -1,6 +1,7 @@
 package com.douzone.mysite.action.board;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -19,22 +20,30 @@ public class MessageAction implements Action {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-		
+
 		HttpSession session = request.getSession();
 		UserVo uservo = (UserVo) session.getAttribute("loginuser");
-		
+
 		String content = request.getParameter("content");
 		String buffer_board_no = request.getParameter("no");
 		int board_no = Integer.parseInt(buffer_board_no);
-		
-		MessageVo vo = new MessageVo();
-		vo.setBoard_no(board_no);
-		vo.setContent(content);
-		vo.setUser_no(uservo.getNo());
-		
-		new MessageDao().Insert(vo);
-		
-		WebUtils.redirect(request, response, request.getContextPath() + "/board?a=view&no="+board_no);
+
+		if (content.equals("")) {
+			PrintWriter out = response.getWriter();
+
+			out.println("<script type=text/javascript>");
+			out.println("alert('제목에 내용이 없습니다!');");
+			out.println("</script>");
+
+		} else {
+			MessageVo vo = new MessageVo();
+			vo.setBoard_no(board_no);
+			vo.setContent(content);
+			vo.setUser_no(uservo.getNo());
+
+			new MessageDao().Insert(vo);
+		}
+		WebUtils.redirect(request, response, request.getContextPath() + "/board?a=view&no=" + board_no);
 	}
 
 }
--- src/com/douzone/mysite/action/board/WriteAction.java
+++ src/com/douzone/mysite/action/board/WriteAction.java
@@ -1,6 +1,7 @@
 package com.douzone.mysite.action.board;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -23,39 +24,56 @@ public class WriteAction implements Action {
 
 		HttpSession session = request.getSession();
 		UserVo uservo = (UserVo) session.getAttribute("loginuser");
-		
+
 		String flag = (String) request.getParameter("reply");
-				
+
 		if (flag == null) {
 			String title = request.getParameter("title");
 			String contents = request.getParameter("content");
 
-			BoardVo vo = new BoardVo();
-			vo.setUser_no(uservo.getNo());
-			vo.setTitle(title);
-			vo.setContents(contents);
+			if (title.equals("")) {
+				PrintWriter out = response.getWriter();
 
-			new BoardDao().insert(vo);
+				out.println("<script type=text/javascript>");
+				out.println("alert('제목에 내용이 없습니다!');");
+				out.println("</script>");
+				
+			} else {
+
+				BoardVo vo = new BoardVo();
+				vo.setUser_no(uservo.getNo());
+				vo.setTitle(title);
+				vo.setContents(contents);
+
+				new BoardDao().insert(vo);
+			}
+
 		} else {
-						
+
 			String buffer_no = request.getParameter("no");
 			int no = Integer.parseInt(buffer_no);
-			
+
 			String title = request.getParameter("title");
 			String contents = request.getParameter("content");
-			
-			BoardVo vo = new BoardVo();
-			vo.setTitle(title);
-			vo.setContents(contents);
-			vo.setUser_no(uservo.getNo());
-			
-			new BoardDao().ReplyInsert(vo,no);
-			
-			
+
+			if (title.equals("")) {
+				PrintWriter out = response.getWriter();
+
+				out.println("<script type=text/javascript>");
+				out.println("alert('제목에 내용이 없습니다!');");
+				out.println("</script>");
+
+			} else {
+
+				BoardVo vo = new BoardVo();
+				vo.setTitle(title);
+				vo.setContents(contents);
+				vo.setUser_no(uservo.getNo());
+
+				new BoardDao().ReplyInsert(vo, no);
+			}
 		}
-
 		WebUtils.redirect(request, response, request.getContextPath() + "/board");
-
 	}
 
 }
